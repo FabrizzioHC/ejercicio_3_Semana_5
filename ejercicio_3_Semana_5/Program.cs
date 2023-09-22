bb04238263da112a795ff9ecaf65d6c930eef7d9
FABRIZZIO <FABRIZZIO@DESKTOP-8PLVDLM>
2023-09-21 23:01:22 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio_3_Semana_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_3_Semana_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingresa el número de nota:");
+            int n = int.Parse(Console.ReadLine());
+            float sumadenotas = 0f;
+
+            // for para bucle
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"ingresa la nota {i}:");
+                float nota = float.Parse(Console.ReadLine());
+                sumadenotas += nota;
+            }
+
+            float promedio = sumadenotas / n;
+            Console.WriteLine($"el promedio es: {promedio}");
+            Console.ReadKey();
+        }
+    }
+}
